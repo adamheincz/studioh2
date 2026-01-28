20a3c038896c08ce0ca08c195be4aa1a5b218113
adamheincz <heinczadam@gmail.com>
2026-01-28 15:12:16 +0100

    Fixed the RouteScroller, beacause it didn't work when tracking id-s where added to the URLs (i.e. Facebook)

--- src/app/routescroller.service.ts
+++ src/app/routescroller.service.ts
@@ -1,6 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { gsap } from 'gsap/gsap-core';
 import { filter } from 'rxjs';
 
@@ -17,9 +17,12 @@ export class RoutescrollerService {
   //   })
   // }
 
-  jumpToSection(section = this.router.url.slice(1)) {
+  sections: string[] = ['rolunk', 'galeria', 'arlista', 'elerhetoseg'];
+
+  jumpToSection(section = this.router.parseUrl(this.router.url).root.children['primary']?.segments[0]?.path) {
+
     if (isPlatformBrowser(this.platformId)) {
-      if (section) {
+      if (section && this.sections.includes(section)) {
         if (section == "elerhetoseg") {
           gsap.to(window, { duration: 1, ease: "power2.out", scrollTo: { y: "max" } });
         } else {
